07145de6f417fcbd7bbc6c096eaaf1b34866e6e2
ansakhar <anna.a.sakharovskaya@student.jyu.fi>
2021-02-08 19:03:27 +0200

    Osa4 Tehtava 4.1

--- osa4/index.js
+++ osa4/index.js
@@ -1,9 +1,52 @@
 const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl =
+  'mongodb+srv://fullstackas:143080Mongo@cluster0.mrl4q.mongodb.net/bloglist?retryWrites=true'
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+  })
+
+app.post('/api/blogs', (request, response) => {
+       
+    /*const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url
+      })*/
+    const blog = new Blog(request.body)
+  
+    blog
+    .save()
+    .then(result => {
+    response.status(201).json(result)
+    })
+  })
 
 const PORT = 3003
 app.listen(PORT, () => {
