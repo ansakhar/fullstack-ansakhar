ed1304f0ec276570f159bab3117fbaa06458970d
ansakhar <anna.a.sakharovskaya@student.jyu.fi>
2021-02-12 11:10:08 +0200

    Osa4 Tehtavat 4.5-4.6

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -24,9 +26,44 @@ const favouriteBlog = (blogs) => {
 
   return blogs.reduce(reducer, 0)
 }
+
+const mostBlogs = (blogs) => {
+
+  return lodash
+      .chain(blogs)
+      .groupBy('author')
+      .map((authorBlogs, currentAuthor) => {
+          return { author: currentAuthor, blogs: authorBlogs.length } })
+      .reduce((a,b) => { if (a.blogs > b.blogs) {
+          return a
+      }
+      return b
+      })
+      .value()
+}
+
+const mostLikes = (blogs) => {
+
+  return lodash
+      .chain(blogs)
+      .groupBy('author')
+      .map((authorBlogs, currentAuthor) => {
+          return { 
+            author: currentAuthor, likes: authorBlogs.map(blog => blog.likes).reduce((a,b) => a + b)
+          } 
+        })
+      .reduce((a,b) => { if (a.likes > b.likes) {
+          return a
+      }
+      return b
+      })
+      .value()
+}
   
   module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
   }
