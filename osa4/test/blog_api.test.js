271d9d9d2773fac39b69976c016566b44de5bbd8
ansakhar <anna.a.sakharovskaya@student.jyu.fi>
2021-03-09 11:52:42 +0200

    Osa4 Tehtava 4.10-4.12

--- osa4/test/blog_api.test.js
+++ osa4/test/blog_api.test.js
@@ -32,6 +32,62 @@ test('blogs are identified by id', async () => {
     expect(contents).toBeDefined()
   })
 
+test('a valid blog can be added ', async () => {
+    const newBlog = {
+        title: "Hiidenkiven puutarhassa",
+        author: "Minna Heinonen",
+        url: "https://hiidenkivenpuutarhassa.blogspot.com/",
+        likes: 205
+      }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    
+      const contents = blogsAtEnd.map(blog => blog.title)
+    expect(contents).toContainEqual('Hiidenkiven puutarhassa')
+  })
+
+  test('a blog without likes adding ', async () => {
+    const newBlog = {
+        title: "Trio MiuMau ja herra Nilsson",
+        author: "Elena Kivinen",
+        url: "https://www.blogit.fi/trio-miumau-ja-herra-nilsson"
+      }
+  
+      const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      const contents = blogsAtEnd.find((blog) => blog.id === response.body.id)
+    expect(contents.likes).toEqual(0)
+  })
+
+test('blog without url is not added', async () => {
+    const newBlog = {
+        title: "Trio MiuMau ja herra Nilsson",
+        author: "Elena Kivinen"
+      }
+  
+    await api
+      .post('/api/notes')
+      .send(newBlog)
+      .expect(404)
+  
+      const blogsAtEnd = await helper.blogsInDb()
+
+      expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+
 afterAll(() => {
   mongoose.connection.close()
 })
