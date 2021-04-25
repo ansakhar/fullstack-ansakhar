6640a075479cd98e249c0e11c544f8b5fea9ca73
ansakhar <anna.a.sakharovskaya@student.jyu.fi>
2021-04-25 10:47:11 +0300

    Osa 6 Tehtava 6.11

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { votePlus } from '../reducers/anecdoteReducer'
-//import { notificationChange } from '../reducers/notificationReducer'
+import { notificationChange } from '../reducers/notificationReducer'
 
 
 const AnecdoteList = () => {
+
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => state.anecdotes)
+  //const notification = useSelector(state => state.notification)
 
   return (
 <div>
+
 {anecdotes.sort((a, b) => (b.votes - a.votes)).map(anecdote =>
     <div key={anecdote.id}>
       <div>
@@ -18,7 +21,14 @@ const AnecdoteList = () => {
       </div>
       <div>
         has {anecdote.votes}
-        <button onClick={() => dispatch(votePlus(anecdote.id))}>vote</button>
+        <button onClick={() => {
+          dispatch(votePlus(anecdote.id))
+          dispatch(notificationChange(`you voted '${anecdote.content}'`))
+          setTimeout(() => {
+            dispatch(notificationChange(null))
+          }, 5000)
+        }
+          }>vote</button>
       </div>
     </div>
   )}
--- osa6/redux-anecdotes/src/components/Notification.js
+++ osa6/redux-anecdotes/src/components/Notification.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+//import { useDispatch } from 'react-redux'
+
+//import { notificationChange } from '../reducers/notificationReducer'
 
 const Notification = (props) => {
 
   const notification = useSelector(state => state.notification)
+  //const dispatch = useDispatch()
 
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+  if (notification === null) {
+    return (
+      <div></div>
+    )
+  }
   return (
+
     <div style={style}>
       {notification}
     </div>
+
   )
 }
 
